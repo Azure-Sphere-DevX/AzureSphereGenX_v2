c575182196ffa26e2ec04798e0eb5138e35eec51
Dave Glover <dglover@microsoft.com>
2021-12-13 21:12:03 +1100

    demo update

--- GenX_Generated/gx_includes/gx_bindings.h
+++ GenX_Generated/gx_includes/gx_bindings.h
@@ -26,34 +26,25 @@ extern const char NETWORK_INTERFACE[];
 /****************************************************************************************
  * Forward declarations
  ****************************************************************************************/
-static DX_DECLARE_DIRECT_METHOD_HANDLER(Counter_gx_handler);
-static DX_DECLARE_DIRECT_METHOD_HANDLER(FactoryLightOff_gx_handler);
-static DX_DECLARE_TIMER_HANDLER(FactoryAlertButtonA_gx_handler);
 
 
 /****************************************************************************************
 * Binding declarations
 ****************************************************************************************/
-extern int count;
-static DX_GPIO_BINDING gpio_FactoryAlertButtonA = { .pin = BUTTON_A, .name = "FactoryAlertButtonA", .direction = DX_INPUT, .detect = DX_GPIO_DETECT_LOW };
-static DX_GPIO_BINDING gpio_FactoryLight = { .pin = STATUS_LED, .name = "FactoryLight", .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = false };
-static DX_DIRECT_METHOD_BINDING dm_FactoryLightOff = { .methodName = "FactoryLightOff", .handler = FactoryLightOff_gx_handler, .context=&gpio_FactoryLight };
-static DX_DIRECT_METHOD_BINDING dm_Counter = { .methodName = "Counter", .handler = Counter_gx_handler, .context=&count };
-static DX_TIMER_BINDING tmr_FactoryAlertButtonA = { .repeat = &(struct timespec){ 0, 200 * ONE_MS }, .delay = NULL, .name = "FactoryAlertButtonA", .handler = FactoryAlertButtonA_gx_handler };
 
 
 // All GPIOs referenced in gpio_bindings with be opened in the gx_initPeripheralAndHandlers function
-static DX_GPIO_BINDING *gpio_bindings[] = { &gpio_FactoryAlertButtonA, &gpio_FactoryLight };
+static DX_GPIO_BINDING *gpio_bindings[] = {  };
 
 // All direct methods referenced in direct_method_bindings will be subscribed to in the gx_initPeripheralAndHandlers function
 static DX_DEVICE_TWIN_BINDING* device_twin_bindings[] = {  };
 
 // All direct methods referenced in direct_method_bindings will be subscribed to in the gx_initPeripheralAndHandlers function
-static DX_DIRECT_METHOD_BINDING *direct_method_bindings[] = { &dm_FactoryLightOff, &dm_Counter };
+static DX_DIRECT_METHOD_BINDING *direct_method_bindings[] = {  };
 
 // All timers referenced in timer_bindings will be opened in the gx_initPeripheralAndHandlers function
 #define DECLARE_DX_TIMER_BINDINGS
-static DX_TIMER_BINDING *timer_bindings[] = { &tmr_FactoryAlertButtonA };
+static DX_TIMER_BINDING *timer_bindings[] = {  };
 
 
 /****************************************************************************************
